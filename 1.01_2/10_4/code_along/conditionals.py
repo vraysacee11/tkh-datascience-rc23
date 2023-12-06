6907f0bb9df2a106599e323bc7c603385e13db66
vraysacee11 <143121928+vraysacee11@users.noreply.github.com>
2023-12-05 21:29:45 -0800

    Add files via upload

---
+++ 1.01_2/10_4/code_along/conditionals.py
@@ -0,0 +1,9 @@
+# TODO: implement conditionals here
+y = 3
+
+if y % 5 == 0:
+	print(“y is divisible by 5”)
+elif  y % 2 == 0:
+	print(“y is divisible by 2”)
+else:
+	print(“y is not divisible by 5 or 2”)
